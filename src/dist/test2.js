ebd11b97c529d0e3547100c880b01accc9d8e4fc
Daniil <daniil@notdiy.studio>
2025-01-22 14:01:36 +0100

    test 2

---
+++ src/dist/test2.js
@@ -0,0 +1,1 @@
+"use strict";const e=require("./assets/index-CAb0ufRs.js");let t=document.querySelector(".test");e.g.to(t,{opacity:0});
---
+++ src/js/test2.js
@@ -0,0 +1,4 @@
+import { gsap } from "gsap";
+
+let testElement = document.querySelector(".test");
+gsap.to(testElement, { opacity: 0 });
