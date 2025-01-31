e97bac535c55376c4c3fa7aa25bbcf5b51a95d3e
Daniil <daniil@notdiy.studio>
2025-01-31 18:27:59 +0100

    thumbnails animation

--- src/js/components/thumbnails.js
+++ src/js/components/thumbnails.js
@@ -1,47 +1,88 @@
 import { gsap } from "gsap";
 
-// Stores event listeners to remove them later during clean up
-const eventListenersMap = new WeakMap();
+// credits: osmo.supply
 
-function smallThumbnailOnHover(thumbnail) {
-  return () =>
-    gsap.to(thumbnail, {
-      y: "-=0.25rem",
-      ease: "back.inOut(1.7)",
-      duration: 0.3,
+function thumbnailOnHover(event) {
+  const magnet = event.currentTarget;
+  const bounding = magnet.getBoundingClientRect();
+  const strength = 25;
+  const innerTarget = magnet.children;
+  const innerStrength = 25;
+
+  const offsetX =
+    ((event.clientX - bounding.left) / magnet.offsetWidth - 0.5) *
+    (strength / 16);
+  const offsetY =
+    ((event.clientY - bounding.top) / magnet.offsetHeight - 0.5) *
+    (strength / 16);
+
+  gsap.to(magnet, {
+    x: offsetX + "em",
+    y: offsetY + "em",
+    rotate: "0.001deg",
+    ease: "power4.out",
+    duration: 0.2,
+  });
+
+  if (innerTarget) {
+    const innerOffsetX =
+      ((event.clientX - bounding.left) / magnet.offsetWidth - 0.5) *
+      (innerStrength / 16);
+    const innerOffsetY =
+      ((event.clientY - bounding.top) / magnet.offsetHeight - 0.5) *
+      (innerStrength / 16);
+
+    gsap.to(innerTarget, {
+      x: innerOffsetX + "em",
+      y: innerOffsetY + "em",
+      rotate: "0.001deg",
+      ease: "power2.out",
+      duration: 0.8,
     });
+  }
 }
 
-function smallThumbnailOffHover(thumbnail) {
-  return () => gsap.to(thumbnail, { y: "", duration: 0.3 });
+function thumbnailOffHover(event) {
+  console.log("mouse leave");
+  const magnet = event.currentTarget;
+  const innerTarget = magnet.firstChild;
+
+  gsap.to(magnet, {
+    x: "0em",
+    y: "0em",
+    ease: "elastic.out(1, 0.3)",
+    duration: 1.6,
+    clearProps: "all",
+  });
+
+  if (innerTarget) {
+    gsap.to(innerTarget, {
+      x: "0em",
+      y: "0em",
+      ease: "elastic.out(1, 0.3)",
+      duration: 2,
+      clearProps: "all",
+    });
+  }
 }
 
 /////////////////////////////////////
 
 const thumbnailsInit = (page) => {
-  let thumbnails = page.querySelectorAll("[placeholder-tag]"); // rewrite later
+  let thumbnails = page.querySelectorAll(".project-thumbnail_image-wrapper");
 
   thumbnails.forEach((thumbnail) => {
-    const hoverInListener = smallThumbnailOnHover(thumbnail);
-    const hoverOutListener = smallThumbnailOffHover(thumbnail);
-
-    eventListenersMap.set(thumbnail, { hoverInListener, hoverOutListener });
-
-    thumbnail.addEventListener("mouseenter", hoverInListener);
-    thumbnail.addEventListener("mouseleave", hoverOutListener);
+    thumbnail.addEventListener("mousemove", thumbnailOnHover);
+    thumbnail.addEventListener("mouseleave", thumbnailOffHover);
   });
 };
 
 const thumbnailsCleanup = (page) => {
-  let thumbnails = page.querySelectorAll("[placeholder-tag]"); // rewrite later
+  let thumbnails = page.querySelectorAll(".project-thumbnail_image-wrapper");
 
   thumbnails.forEach((thumbnail) => {
-    const listeners = eventListenersMap.get(thumbnail);
-    if (listeners) {
-      thumbnail.removeEventListener("mouseenter", listeners.hoverInListener);
-      thumbnail.removeEventListener("mouseleave", listeners.hoverOutListener);
-      eventListenersMap.delete(thumbnail);
-    }
+    thumbnail.removeEventListener("mousemove", thumbnailOnHover);
+    thumbnail.removeEventListener("mouseleave", thumbnailOffHover);
   });
 };
 
