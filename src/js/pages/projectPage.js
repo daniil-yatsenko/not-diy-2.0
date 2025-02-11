6df38752a617f35e0f255cc5d60beead33747c39
Daniil <daniil@notdiy.studio>
2025-02-11 18:00:49 +0100

    wip

--- src/js/pages/projectPage.js
+++ src/js/pages/projectPage.js
@@ -1,18 +1,10 @@
 import { gsap } from "gsap";
 import { defaultLeave, defaultEnter } from "../global/globalTransitions";
+import { thumbnailsCleanup } from "../components/thumbnails";
 
 const overlay = document.querySelector(".overlay");
 const loader = overlay.querySelector(".loader");
 const navbar = document.querySelector(".navbar");
-
-function getPosition(el) {
-  const rect = el.getBoundingClientRect();
-  return {
-    x: rect.left,
-    y: rect.top,
-    height: rect.height,
-  };
-}
 
 const projectLeave = async (data) => {
   const currentPage = data.current.container;
@@ -23,28 +15,45 @@ const projectLeave = async (data) => {
   if (!currentImage) return defaultLeave();
 
   let tl = gsap.timeline();
-  const tempImage = document.createElement("div");
-  tempImage.appendChild(currentImage.cloneNode(true));
-  overlay.appendChild(tempImage);
+  thumbnailsCleanup(currentPage);
 
-  tl.set(tempImage, {
-    opacity: 0,
-    position: "fixed",
-    zIndex: "10",
-    left: getPosition(currentImage).x - getPosition(currentPage).x,
-    top: getPosition(currentImage).y - getPosition(currentPage).y,
-  });
-  tl.set(loader, { display: "none", opacity: "" });
+  tl.set(loader, { display: "flex", opacity: "" });
   tl.set(overlay, { y: "-120vh", display: "block" });
-  tl.to(navbar, { y: "-100%", ease: "expo.inOut", duration: 0.5 });
+  tl.to(currentImage, { x: "-100vw", duration: 0.75, ease: "expo.inOut" }, "<");
+  tl.to(navbar, { y: "-100%", ease: "expo.inOut", duration: 0.5 }, "<");
   tl.to(overlay, { y: "", duration: 1, delay: -0.4, ease: "expo.inOut" });
-  tl.to(tempImage, { opacity: 1 });
 
   tl.set(navbar, { y: "" });
 
   return tl.then(() => {});
 };
 
-const projectEnter = async (data) => {};
+const projectEnter = async (data) => {
+  const nextPage = data.next.container;
+  const image = nextPage.querySelector("[cover-image-desktop]");
+  let tl = gsap.timeline();
+
+  tl.set(overlay, { y: "", display: "block" });
+  tl.set(navbar, { y: "-100%" });
+
+  tl.set(image, { x: "100vw", duration: 0.75, ease: "expo.inOut" });
+  tl.to(
+    overlay,
+    { y: "100vh", duration: 1, ease: "expo.inOut", delay: 0.3 },
+    "<"
+  );
+  tl.to(image, { x: "", duration: 0.75, ease: "expo.inOut", delay: -0.5 });
+  tl.to(navbar, {
+    y: "",
+    ease: "expo.inOut",
+    duration: 0.5,
+    delay: -0.2,
+  });
+
+  tl.set(loader, { display: "none", opacity: "" });
+  tl.set(overlay, { opacity: 1, y: "", display: "none" });
+
+  return tl.then(() => {});
+};
 
 export { projectEnter, projectLeave };
