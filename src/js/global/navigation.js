de4286137039b7bb59807a331756664009fb8d1e
Daniil <daniil@notdiy.studio>
2025-02-19 13:51:15 +0100

    mobile menu open

--- src/js/global/navigation.js
+++ src/js/global/navigation.js
@@ -1,5 +1,65 @@
 import { gsap } from "gsap";
 
+const mobileMenu = () => {
+  const menuBtn = document.querySelector(".navbar_nav-link.is-mobile-menu");
+  const linksWrapper = document.querySelector(".navbar_menu-wrapper");
+  const links = linksWrapper.querySelectorAll(".navbar_nav-link");
+  const overlay = document.querySelector(".navbar_overlay");
+  const figure = document.querySelector(".navbar_overlay_figure-wrapper");
+  let isMenuOpen = false;
+
+  gsap.set(linksWrapper, { display: "none" });
+  gsap.set(overlay, { display: "none" });
+
+  const openMenu = () => {
+    console.log("open menu");
+    const tl = gsap.timeline();
+    tl.set([menuBtn, overlay], { pointerEvents: "none" });
+    tl.set(overlay, { display: "block", opacity: 0 });
+    tl.set(links, { x: "10rem" });
+    tl.set(linksWrapper, { display: "flex" });
+    tl.to(overlay, { opacity: 1 });
+    tl.to(
+      figure,
+      {
+        x: "-12rem",
+        rotate: -40,
+        duration: 0.5,
+        ease: "expo.inOut",
+      },
+      "<"
+    );
+    tl.to(links, {
+      x: "0rem",
+      duration: 0.5,
+      ease: "expo.inOut",
+      stagger: 0.1,
+      delay: -0.4,
+    });
+    tl.set([menuBtn, overlay], { pointerEvents: "" });
+    isMenuOpen = true;
+  };
+
+  const closeMenu = () => {
+    console.log("close menu");
+    const tl = gsap.timeline();
+    tl.set([menuBtn, overlay], { pointerEvents: "none" });
+    tl.set(linksWrapper, { display: "none" });
+    tl.set(overlay, { display: "none" });
+    tl.set(figure, { x: "0rem", rotate: 0 });
+    tl.set([menuBtn, overlay], { pointerEvents: "" });
+    isMenuOpen = false;
+  };
+
+  menuBtn.addEventListener("click", () => {
+    isMenuOpen ? closeMenu() : openMenu();
+  });
+
+  overlay.addEventListener("click", () => {
+    closeMenu();
+  });
+};
+
 const navbarSelector = () => {
   const navbar = document.querySelector(".navbar_menu-wrapper");
   const navLinks = navbar.querySelectorAll(".navbar_nav-link");
@@ -19,7 +79,7 @@ const navbarSelector = () => {
 
     const nextLinkDimensions = {
       width: nextLinkRect.width,
-      height: nextLinkRect.height,
+      height: nextLinkRect.height + 1,
       top: nextLinkRect.top - parentRect.top,
       left: nextLinkRect.left - parentRect.left,
     };
@@ -100,4 +160,4 @@ const navbarSelector = () => {
   });
 };
 
-export { navbarSelector };
+export { navbarSelector, mobileMenu };
